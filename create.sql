1616f70070a5229cffdea5548df8302f1ac9d2d2
TGMikele <ikeles@students.tgm.ac.at>
2016-04-15 22:16:54 +0200

    verbesserte abgabe

--- create.sql
+++ create.sql
@@ -7,9 +7,9 @@
 
 
 CREATE TABLE Person ( --df: mult=1.0
- persnr Serial NOT NULL, --df: offset=10000 step=2 size=99999 
- vname VARCHAR(55), --df: text=vorname
- nname VARCHAR(55), --df: text=nachname
+ persnr Serial NOT NULL, --df: offset=10000 step=2 size=99999
+ vname VARCHAR(55), --df: text=vorname length=1
+ nname VARCHAR(55), --df: text=nachname length=1
  geschlecht CHAR(1),--df: pattern='(M\W)'
  gebdate DATE --df: date
 );
